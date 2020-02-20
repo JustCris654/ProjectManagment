e2a949756568b1b730210578b15fff61ee69a480
JustCris654 <59602675+JustCris654@users.noreply.github.com>
2020-02-20 10:13:42 +0100

    Add files via upload

---
+++ ProjectManagmentForm/DeterministicActivity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ProjectManagmentForm 
+{
+	class DeterministicActivity : Activity
+    {
+		private TimeSpan _duration;
+
+
+        #region Costruttori
+        public DeterministicActivity( int id, string name, string description, Date startDate, TimeSpan duration ) 
+			: base(id, name, description, startDate, Date.Today) {
+			_duration = duration;
+            _endDate = _startDate.AddDays(_duration.Days - 1);
+		}
+        public DeterministicActivity( int id, string name, string description) : this(id, name, description, Date.Today, TimeSpan.Zero) { }
+        public DeterministicActivity( ) : base()
+        {
+			_duration = TimeSpan.Zero;
+		}
+        #endregion
+
+        
+	}
+
+}
---
+++ ProjectManagmentForm/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace ProjectManagmentForm {
+	public partial class Form1 : Form {
+
+		StreamWriter _stdOutWriter;
+		public Form1( ) {
+			InitializeComponent( );
+		}
+
+		private void Form1_Load( object sender, EventArgs e ) {
+			//var stdout = Console.OpenStandardOutput();
+			//_stdOutWriter = new StreamWriter(stdout);
+			//_stdOutWriter.AutoFlush = true;
+			//AllocConsole( );
+			//_stdOutWriter.WriteLine("Ciao xoxo");
+
+		}
+		//[DllImport("kernel32.dll", SetLastError = true)]
+		//[return: MarshalAs(UnmanagedType.Bool)]
+		//static extern bool AllocConsole( );
+	}
+}
